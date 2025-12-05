547b316f2110abc139ebfe085791e84c71dd7551
Camila <132607769+CAMILAB17@users.noreply.github.com>
2025-12-04 21:15:39 -0300

    Subo archivos

---
+++ js/index.js
@@ -0,0 +1,49 @@
+import { agregarAlCarrito } from "./funcionesCarrito.js";
+import { obtenerCarrito } from "./storage.js";
+import { actualizarContador } from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contenedor = document.getElementById("contenedor-tarjetas");
+    const carrito = obtenerCarrito();
+    actualizarContador(carrito);
+
+    fetch("./data/productos.json").then(res => {
+        if (!res.ok) {
+            throw new Error(`Error HTTP status: ${res.status}`);
+        }
+        return res.json()
+    })
+    .then((data) => {
+        data.forEach(producto => {
+            const tarjeta = document.createElement("article")
+            tarjeta.classList.add("tarjeta-producto")
+
+            const titulo = document.createElement("h4")
+            titulo.textContent = producto.nombre
+
+            const img = document.createElement("img")
+            img.alt = producto.nombre 
+            img.src = `./${producto.img}`
+
+            const precio = document.createElement("p")
+            precio.textContent = `$${producto.precio}`
+
+            const boton = document.createElement("button")
+            boton.classList.add("btn")
+            boton.textContent = "Agregar al carrito"
+
+            boton.addEventListener("click", () => {
+                agregarAlCarrito(producto)
+             })
+
+            tarjeta.appendChild(img)
+            tarjeta.appendChild(titulo)
+            tarjeta.appendChild(precio)
+            tarjeta.appendChild(boton)
+
+            contenedor.appendChild(tarjeta)
+
+        });
+    }).catch((error) => console.error("Error al cargar los productos",error));
+
+});
